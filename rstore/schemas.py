f256cd91940e87689fa643c055f8f11f6b779ea4
Cédric Bonhomme <cedric@cedricbonhomme.org>
2024-07-31 21:56:52 +0200

    new: [schemas] Added initial Pydantic schemas (from SCANDALE).

---
+++ rstore/schemas.py
@@ -0,0 +1,22 @@
+import uuid
+
+from pydantic import BaseModel
+
+class Payload(BaseModel):
+    raw: str
+
+
+class Meta(BaseModel):
+    uuid: str
+    ts: int
+    type: str
+
+
+class ScanData(BaseModel):
+    version: str
+    format: str
+    meta: Meta
+    payload: Payload
+
+    class Config:
+        from_attributes = True
